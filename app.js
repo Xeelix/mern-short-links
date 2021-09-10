266cf918b896a2f7cb127ac33c92dc418d3d72dd
xeelix <fritlangoof@gmail.com>
2021-09-10 15:03:18 +0700

    Changed port

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-const PORT = config.get('port') || 5000
+const PORT = 80
 
 app.post("/test", function (req, res) {
     res.send(req.body);
